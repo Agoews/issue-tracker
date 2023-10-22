0bb5816255019ed9f55dc9d4469bd0a16b1399ca
George Halterman <113994972+Agoews@users.noreply.github.com>
2023-10-21 21:18:33 -0700

    disabled ssr markdown editor

--- app/issues/[id]/loading.tsx
+++ app/issues/[id]/loading.tsx
@@ -1,7 +1,24 @@
-import React from "react";
+import IssueStatusBadge from "@/app/components/IssueStatusBadge";
+import { Heading, Flex, Card, Box } from "@radix-ui/themes";
+import ReactMarkdown from "react-markdown";
+import Skeleton from "react-loading-skeleton";
+import "react-loading-skeleton/dist/skeleton.css";
 
 const LoadingIssuePage = () => {
-  return <div>Loading...</div>;
+  return (
+    <Box className="mx-5">
+      <Skeleton width="10rem" />
+      <Flex gap="3" my="2">
+        <Skeleton width="4rem" />
+        <Skeleton width="8rem" />
+      </Flex>
+      <Card className="prose">
+        <Skeleton />
+        <Skeleton />
+        <Skeleton />
+      </Card>
+    </Box>
+  );
 };
 
 export default LoadingIssuePage;
